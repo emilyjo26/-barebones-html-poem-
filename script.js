693c870783db21125f58a6274dcb54deb77210f9
emilyjo26 <89491613+emilyjo26@users.noreply.github.com>
2021-10-15 08:51:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-}
+function myFunction 
+
+var element = document.getElementById("button");
+  element.classList.toggle("dark-mode");
+  button.addEventListener(onclick, dark-mode). 
